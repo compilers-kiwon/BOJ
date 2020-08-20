e77bb1adb6f4cae6b8769ab4b9110c9a52cd3296
Kiwon Kwon <compilers.naver.com>
2020-08-20 15:04:28 +0900

    https://www.acmicpc.net/problem/2523

---
+++ 02523_Star/star.cpp
@@ -0,0 +1,30 @@
+#include	<iostream>
+
+using namespace	std;
+
+int	main(void)
+{
+	int	N;
+	
+	cin>>N;
+	
+	for(int i=1;i<=N;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<'*';
+		}
+		cout<<endl;
+	}
+	
+	for(int i=N-1;i>=1;i--)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<'*';
+		}
+		cout<<endl;
+	}
+	
+	return	0;
+}
