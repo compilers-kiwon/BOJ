1a838455b38ef46d1bf1a5c786060ae496b05663
Kiwon Kwon <compilers.naver.com>
2020-09-30 20:43:18 +0900

    https://www.acmicpc.net/board/view/50678

--- 10000-10999/10211_Max_Subarr/max.cpp
+++ 10000-10999/10211_Max_Subarr/max.cpp
@@ -1,10 +1,16 @@
 #include	<iostream>
-#include	<vector>
 
 using namespace	std;
 
+#define	MAX_SIZE	(1000+1)
+
+int	X[MAX_SIZE],dp[MAX_SIZE];
+
 int	main(void)
 {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+	
 	int	T;
 	
 	cin>>T;
@@ -12,27 +18,25 @@ int	main(void)
 	while(T>0)
 	{
 		int			N,max_sub_sum;
-		vector<int>	X,dp;
-		
+
 		cin>>N;
-		
-		X.resize(N+1);
-		dp.resize(N+1);
-		
+
 		for(int i=1;i<=N;i++)
 		{
 			cin>>X[i];
 		}
 		
+		dp[0] = 0;
 		dp[1] = X[1];
 		max_sub_sum = -1001;
 		
-		for(int i=2;i<=N;i++)
+		for(int i=1;i<=N;i++)
 		{
 			dp[i] = max(X[i],dp[i-1]+X[i]);
 			max_sub_sum = max(max_sub_sum,dp[i]);
 		}
-		cout<<max_sub_sum<<endl;
+		
+		cout<<max_sub_sum<<'\n';
 		
 		T--;
 	}
