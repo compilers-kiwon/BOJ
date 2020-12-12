ce14705be821e07a02511c55890ad7dc48994da0
compilers-kiwon <compilers@naver.com>
2020-12-12 12:10:35 +0900

    https://www.acmicpc.net/problem/1655, https://www.acmicpc.net/board/view/60119

--- 01000-01999/01655_Middle/middle.cpp
+++ 01000-01999/01655_Middle/middle.cpp
@@ -1,37 +1,84 @@
 #include	<iostream>
-#include	<algorithm>
-#include	<vector>
+#include	<queue>
 
 using namespace	std;
 
+typedef	priority_queue<int>	pq;
+
+int	N;
+pq	left_q,right_q;
+
 int	main(void)
 {
-	int			N;
-	vector<int>	numbers;
-	
-	scanf("%d",&N);
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	cin>>N;
 	
 	for(int i=1;i<=N;i++)
 	{
-		int	n,middle;
+		int	n,tmp;
 		
-		scanf("%d",&n);
+		cin>>n;
 		
-		vector<int>::iterator	it;
-		
-		it = upper_bound(numbers.begin(),numbers.end(),n);
-		numbers.insert(it,n);
-		
-		if( i%2 == 0 )
+		if( left_q.empty() || right_q.empty() )
 		{
-			middle = i/2-1;
+			if( left_q.empty() )
+			{
+				left_q.push(n);
+			}
+			else // right_q.empty()
+			{
+				if( n < left_q.top() )
+				{
+					tmp = left_q.top();
+					left_q.pop();
+					
+					left_q.push(n);
+					right_q.push(-tmp);
+				}
+				else
+				{
+					right_q.push(-n);
+				}
+			}
+			
+			cout<<left_q.top()<<'\n';
+			continue;
+		}
+		
+		if( left_q.size() == right_q.size() )
+		{
+			if( n<=left_q.top() || n<=-right_q.top() )
+			{
+				left_q.push(n);
+			}
+			else
+			{
+				tmp = -right_q.top();
+				right_q.pop();
+				
+				left_q.push(tmp);
+				right_q.push(-n);
+			}
 		}
 		else
 		{
-			middle = i/2;
+			if( n>=-right_q.top() || n>=left_q.top() )
+			{
+				right_q.push(-n);
+			}
+			else
+			{
+				tmp = left_q.top();
+				left_q.pop();
+				
+				right_q.push(-tmp);
+				left_q.push(n);
+			}
 		}
 		
-		printf("%d\n",numbers[middle]);
+		cout<<left_q.top()<<'\n';
 	}
 	
 	return	0;
