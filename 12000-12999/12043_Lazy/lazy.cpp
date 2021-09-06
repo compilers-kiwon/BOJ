4c42bb232c39f928e3ec3c3e001dccbb8378a50f
compilers-kiwon <compilers@naver.com>
2021-09-06 11:27:10 +0900

    https://www.acmicpc.net/problem/12043 => optimize

--- 12000-12999/12043_Lazy/lazy.cpp
+++ 12000-12999/12043_Lazy/lazy.cpp
@@ -1,58 +1,54 @@
 #include    <iostream>
 #include    <string>
-#include    <set>
 
 using namespace std;
 
-#define MAX_SIZE    1000
-#define MOD(n)      ((n)%1000000007)
-#define TBD         -1
+#define MOD(n)              ((n)%1000000007)
+#define IN_RANGE(MIN,n,MAX) ((MIN)<=(n)&&(n)<=(MAX))
 
-int dp[0x100][MAX_SIZE];
-
-int     get_dp(const string& str,char c,int pos)
+int get_num_of_candidates(const string& str,int pos)
 {
-    int&    ret = dp[(int)c][pos];
+    int     ret = 0;
+    bool    counted[0x100];
 
-    if( ret != TBD )
+    fill(&counted['a'],&counted['z'+1],false);
+
+    for(int offset=-1;offset<=1;offset++)
     {
-        return  ret;
-    }
-
-    // pos,pos-1,pos-2
-    set<char>   p;
-
-    for(int i=0;i<=2;i++)
-    {
-        if( pos-i < 0 )
+        if( !IN_RANGE(0,pos+offset,str.length()-1) || counted[str[pos+offset]]==true )
         {
-            break;
+            continue;
         }
 
-        p.insert(str[pos-i]);
-    }
-
-    set<char>::iterator it;
-
-    for(ret=0,it=p.begin();it!=p.end();it++)
-    {
-        ret = MOD(ret+get_dp(str,*it,pos-1));
+        counted[str[pos+offset]] = true;
+        ret++;
     }
 
     return  ret;
 }
 
-void    init(const string& str)
+int get_dp(const string& str,int pos)
 {
-    for(char c='a';c<='z';c++)
+    if( pos == -1 )
     {
-        fill(&dp[(int)c][0],&dp[(int)c][str.length()],TBD);
+        return  1;
     }
 
-    dp[str[0]][0] = dp[str[1]][0] = 1;
+    int ret,n,dp;
+    
+    ret = 0;
+    n = get_num_of_candidates(str,pos);
+    dp = get_dp(str,pos-1);
+
+    for(int i=1;i<=n;i++)
+    {
+        ret = MOD(ret+dp);
+    }
+
+    return  ret;
 }
 
-int     main(void)
+int main(void)
 {
     cin.tie(NULL);
     cin.sync_with_stdio(false);
@@ -66,29 +62,7 @@ int     main(void)
         string  str;
 
         cin>>str;
-        cout<<"Case #"<<t<<": ";
-
-        if( str.length() == 1 )
-        {
-            cout<<"1\n";
-            continue;
-        }
-
-        int w1,w2;
-
-        init(str);
-        w1 = get_dp(str,str[str.length()-1],str.length()-1);
-
-        if( str[str.length()-1] != str[str.length()-2] )
-        {
-            w2 = get_dp(str,str[str.length()-2],str.length()-1);
-        }
-        else
-        {
-            w2 = 0;
-        }
-        
-        cout<<MOD(w1+w2)<<'\n';
+        cout<<"Case #"<<t<<": "<<get_dp(str,str.length()-1)<<'\n';
     }
 
     return  0;
