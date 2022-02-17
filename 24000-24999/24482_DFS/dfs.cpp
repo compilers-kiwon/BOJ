893b4566abbeec006925c280302d60e226cd8bb4
compilers-kiwon <compilers@naver.com>
2022-02-17 16:48:19 +0900

    https://www.acmicpc.net/problem/24482

---
+++ 24000-24999/24482_DFS/dfs.cpp
@@ -0,0 +1,70 @@
+#include    <iostream>
+#include    <vector>
+#include    <algorithm>
+
+using namespace std;
+
+#define MAX_SIZE    (100000+1)
+#define NONE        -1
+
+int         N,M,R,depth[MAX_SIZE];
+vector<int> connected[MAX_SIZE];
+
+bool    is_bigger(const int& a,const int& b)
+{
+    return  a>b;
+}
+
+void    input(void)
+{
+    cin>>N>>M>>R;
+
+    for(int i=1;i<=M;i++)
+    {
+        int u,v;
+
+        cin>>u>>v;
+
+        connected[u].push_back(v);
+        connected[v].push_back(u);
+    }
+
+    for(int i=1;i<=N;i++)
+    {
+        depth[i] = NONE;
+        sort(connected[i].begin(),connected[i].end(),is_bigger);
+    }
+}
+
+void    dfs(int current,int d)
+{
+    if( depth[current] != NONE )
+    {
+        return;
+    }
+
+    depth[current] = d;
+
+    vector<int>&    adj = connected[current];
+
+    for(int i=0;i<adj.size();i++)
+    {
+        dfs(adj[i],d+1);
+    }
+}
+
+int     main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    input();
+    dfs(R,0);
+
+    for(int i=1;i<=N;i++)
+    {
+        cout<<depth[i]<<'\n';
+    }
+
+    return  0;
+}
