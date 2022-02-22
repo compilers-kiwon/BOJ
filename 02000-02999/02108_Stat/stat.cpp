b3b3fdff75938cd8b64cf1586cb8a18032128b05
compilers-kiwon <compilers@naver.com>
2022-02-23 00:04:25 +0900

    https://www.acmicpc.net/board/view/83995

--- 02000-02999/02108_Stat/stat.cpp
+++ 02000-02999/02108_Stat/stat.cpp
@@ -1,62 +1,55 @@
 #include	<iostream>
 #include	<algorithm>
+#include	<cmath>
+#include	<vector>
+#include	<map>
 
 using namespace	std;
 
-#define	MAX_NUMS	500000
-#define	MIN_NUM		-4000
-#define	MAX_NUM		4000
-
-#define	GET_INDEX(n)	((n)+MAX_NUM)
-
-int	num[MAX_NUMS],cnt[MAX_NUM-MIN_NUM+1];
-
 int	main(void)
 {
-	int	N,max_num,min_num,sum,avg;
-	
-	scanf("%d",&N);
-	
-	max_num = MIN_NUM-1;
-	min_num = MAX_NUM+1;
+	int				N,sum;
+	vector<int>		num;
+	map<int,int>	count_table;
+
+	cin>>N;
 	sum = 0;
-	
-	for(int i=0;i<MAX_NUM-MIN_NUM+1;i++)
+
+	for(int i=1;i<=N;i++)
 	{
-		cnt[i] = 0;
+		int	n;
+
+		cin>>n;
+		sum += n;
+
+		num.push_back(n);
+		count_table[n]++;
 	}
-	
-	for(int i=0;i<N;i++)
+
+	cout<<(int)round((double)sum/(double)N)<<'\n';
+
+	sort(num.begin(),num.end());
+	cout<<num[N/2]<<'\n';
+
+	int			max_cnt = 0;
+	vector<int>	mode;
+
+	for(map<int,int>::iterator it=count_table.begin();it!=count_table.end();it++)
 	{
-		scanf("%d",&num[i]);
-		sum += num[i];
-		max_num = max(max_num,num[i]);
-		min_num = min(min_num,num[i]);
-		cnt[GET_INDEX(num[i])]++;
-	}
-	sort(&num[0],&num[N]);
-	
-	int		max_cnt,mode,cnt_of_max_cnt;
-	
-	max_cnt = 0;
-	cnt_of_max_cnt = 0;
-	
-	for(int i=MIN_NUM;i<=MAX_NUM;i++)
-	{
-		if( cnt[GET_INDEX(i)] > max_cnt )
+		if( it->second >= max_cnt )
 		{
-			max_cnt = cnt[GET_INDEX(i)];
-			mode = i;
-			cnt_of_max_cnt = 1;
-		}
-		else if( cnt[GET_INDEX(i)] == max_cnt && cnt_of_max_cnt == 1 )
-		{
-			mode = i;
-			cnt_of_max_cnt++;
+			if( it->second > max_cnt )
+			{
+				mode.clear();
+			}
+
+			max_cnt = it->second;
+			mode.push_back(it->first);
 		}
 	}
-	
-	printf("%.0lf\n%d\n%d\n%d\n",(double)sum/(double)N,num[N/2],mode,max_num-min_num);
-		
+
+	cout<<(mode.size()==1?mode[0]:mode[1])<<'\n';
+	cout<<num.back()-num.front()<<'\n';
+
 	return	0;
 }
