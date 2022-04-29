80cf8ee62c52145164c2376576835bfa9424701f
Kiwon Kwon <compilers@naver.com>
2022-04-29 18:35:20 +0900

    https://www.acmicpc.net/problem/11520

---
+++ 11000-11999/11520_Pi/pi.cpp
@@ -0,0 +1,23 @@
+#include    <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int N;
+
+    cin>>N;
+
+    for(int i=1;i<=N;i++)
+    {
+        int P,D;
+
+        cin>>P>>D;
+        cout<<D<<' '<<5<<'\n';
+    }
+
+    return  0;
+}
