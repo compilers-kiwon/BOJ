14bb7c3c8b8c4bd0c4b7598e0da0dfda7fcdfefa
compilers-kiwon <compilers@naver.com>
2022-05-30 17:50:08 +0900

    https://www.acmicpc.net/problem/21983

---
+++ 21000-21999/21983_Basalt/basalt.cpp
@@ -0,0 +1,14 @@
+#include    <iostream>
+#include    <cmath>
+
+using namespace std;
+
+int main(void)
+{
+    double  a;
+
+    scanf("%lf",&a);
+    printf("%.06f\n",6*sqrt(a*2.0/(3.0*sqrt(3.0))));
+
+    return  0;
+}
