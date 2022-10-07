c32a789f3487496f81ceaff9f91bb37e9eaac9aa
Kiwon Kwon <compilers@naver.com>
2022-10-07 23:25:58 +0900

    using BFS

--- 01000-01999/01976_Travel/travel.cpp
+++ 01000-01999/01976_Travel/travel.cpp
@@ -1,74 +1,96 @@
-#include	<iostream>
+#include    <iostream>
+#include    <vector>
+#include    <queue>
 
-using namespace	std;
+using namespace std;
 
-#define	MAX_NUM_OF_CITY	(200+1)
+#define MAX_SIZE    (200+1)
 
-#define	TRUE	1
-#define	FALSE	0
+int         N,M,path[MAX_SIZE][MAX_SIZE];
+bool        visited[MAX_SIZE];
+vector<int> plan_to_visit,connected[MAX_SIZE];
 
-int	map[MAX_NUM_OF_CITY][MAX_NUM_OF_CITY];
-
-int	main(void)
+void    input(void)
 {
-	int	N,M;
-	
-	cin>>N>>M;
-	
-	for(int i=1;i<=N;i++)
-	{
-		for(int j=1;j<=N;j++)
-		{
-			cin>>map[i][j];
-		}
-		map[i][i] = TRUE;
-	}
-	
-	for(int k=1;k<=N;k++)
-	{
-		for(int i=1;i<=N;i++)
-		{
-			for(int j=1;j<=N;j++)
-			{
-				if( map[i][j] == FALSE )
-				{
-					if( map[i][k] == TRUE && map[k][j] == TRUE )
-					{
-						map[i][j] = TRUE;
-					}
-				}
-			}
-		}
-	}
-	
-	int	prev,result;
-	
-	cin>>prev;
-	result = TRUE;
-	
-	for(int i=2;i<=M;i++)
-	{
-		int	next;
-		
-		cin>>next;
-		
-		if( map[prev][next] == FALSE )
-		{
-			result = FALSE;
-			break;
-		}
-		
-		prev = next;
-	}
-	
-	if( result == TRUE )
-	{
-		cout<<"YES\n";
-	}
-	else
-	{
-		cout<<"NO\n";
-	}
-	
-	return	0;
+    cin>>N>>M;
+
+    for(int row=1;row<=N;row++)
+    {
+        for(int col=1;col<=N;col++)
+        {
+            cin>>path[row][col];
+        }
+    }
+
+    for(int i=1;i<=M;i++)
+    {
+        int city;
+
+        cin>>city;
+        plan_to_visit.push_back(city);
+    }
+}
+
+void    get_connected(void)
+{
+    for(int row=1;row<=N;row++)
+    {
+        for(int col=row+1;col<=N;col++)
+        {
+            if( path[row][col] == 1 )
+            {
+                connected[row].push_back(col);
+                connected[col].push_back(row);
+            }
+        }
+    }
+}
+
+void    bfs(int s)
+{
+    queue<int>  q;
+
+    for(q.push(s),visited[s]=true;!q.empty();q.pop())
+    {
+        int current_city = q.front();
+        vector<int>&    adj = connected[current_city];
+
+        for(int i=0;i<adj.size();i++)
+        {
+            int&    adj_city = adj[i];
+
+            if( visited[adj_city] == false )
+            {
+                q.push(adj_city);
+                visited[adj_city] = true;
+            }
+        }
+    }
+}
+
+bool    check_if_visitable(void)
+{
+    for(int i=0;i<plan_to_visit.size();i++)
+    {
+        if( visited[plan_to_visit[i]] == false )
+        {
+            return  false;
+        }
+    }
+
+    return  true;
+}
+
+int     main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    input();
+    get_connected();
+
+    bfs(plan_to_visit.front());
+    cout<<(check_if_visitable()?"YES":"NO")<<'\n';
+
+    return  0;
 }
