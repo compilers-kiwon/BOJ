408a444440c1604089a834d0a4cd2eb6fb6b9cf7
compilers-kiwon <kkwon@uei.com>
2023-01-06 11:49:10 +0900

    https://www.acmicpc.net/problem/13597

---
+++ 13000-13999/13597_Tri-du/tri-du.cpp
@@ -0,0 +1,16 @@
+#include    <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int A,B;
+
+    cin>>A>>B;
+    cout<<max(A,B)<<'\n';
+
+    return  0;
+}
