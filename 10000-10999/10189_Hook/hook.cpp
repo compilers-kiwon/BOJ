dbf4e8a3a4b4c90a4785e54817f96907c4cb752b
Kiwon Kwon <compilers@naver.com>
2023-01-25 23:41:02 +0900

    https://www.acmicpc.net/problem/10189

---
+++ 10000-10999/10189_Hook/hook.cpp
@@ -0,0 +1,16 @@
+#include    <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    cout<<"#  # #### #### #  #\n";
+    cout<<"#### #  # #  # # #\n";
+    cout<<"#### #  # #  # # #\n";
+    cout<<"#  # #### #### #  #\n";
+
+    return  0;
+}
