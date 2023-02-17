fefd5d7aca29fa7589419319a017a1db6c5601d8
Kiwon Kwon <compilers@naver.com>
2023-02-17 19:47:00 +0900

    https://www.acmicpc.net/board/view/108298

--- 14000-14999/14426_Prefix/prefix.cpp
+++ 14000-14999/14426_Prefix/prefix.cpp
@@ -1,76 +1,71 @@
 #include	<iostream>
 #include	<string>
 #include	<vector>
+#include	<set>
 
 using namespace	std;
 
-#define	MAX_ROW	26
-#define	MAX_COL	500
+static int	N,M;
+static vector<string> prefix;
+static set<string>	words;
 
-#define	GET_ROW_INDEX(str)	((int)((str)[0]-'a'))
-#define	GET_COL_INDEX(str)	((str).length())
+#define	TRUE	1
+#define	FALSE	0
 
-vector<string>	table[MAX_ROW][MAX_COL+1];
+#define	is_found(i,s)		((i)!=(s).end())
+#define	is_shorter(w,p)		((w).length()<(p).length())
+#define	is_included(w,p)	((w).substr(0,(p).length())==(p))
+
+int	input(void)
+{
+	cin>>N>>M;
+
+	for(int i=1;i<=N;i++)
+	{
+		string	str;
+
+		cin>>str;
+		words.insert(str);
+	}
+
+	for(int i=1;i<=M;i++)
+	{
+		string	str;
+
+		cin>>str;
+		prefix.push_back(str);
+	}
+
+	return	0;
+}
+
+int	find_prefix_word(const string& p)
+{
+	set<string>::iterator	it = words.lower_bound(p);
+
+	if( !is_found(it,words) || is_shorter(*it,p) )
+	{
+		return	FALSE;
+	}
+
+	return	(is_included(*it,p)?TRUE:FALSE);
+}
 
 int	main(void)
 {
-	int	N,M,cnt;
-	
-	cin>>N>>M;
-	
-	for(int i=1;i<=N;i++)
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	int	cnt = 0;
+
+	input();
+
+	for(int i=0;i<prefix.size();i++)
 	{
-		string	s;
-		
-		cin>>s;
-		table[GET_ROW_INDEX(s)][GET_COL_INDEX(s)].push_back(s);
+		cnt += find_prefix_word(prefix[i]);
 	}
-	
-	cnt = 0;
-	
-	for(int i=1;i<=M;i++)
-	{
-		string	s;
-		int		row_index,col_index;
-		bool	found;
-		
-		cin>>s;
-		row_index = GET_ROW_INDEX(s);
-		col_index = GET_COL_INDEX(s);
-		found = false;
-		
-		for(int n=col_index;n<=MAX_COL;n++)
-		{
-			vector<string>&	words = table[row_index][n];
-			
-			for(int j=0;j<words.size();j++)
-			{
-				found = true;
-				
-				for(int k=0;k<col_index;k++)
-				{
-					if( words[j][k] != s[k] )
-					{
-						found = false;
-						break;
-					}
-				}
-				
-				if( found == true )
-				{
-					break;
-				}
-			}
-			
-			if( found == true )
-			{
-				cnt++;
-				break;
-			}
-		}
-	}
-	
+
 	cout<<cnt<<'\n';
-	
+
 	return	0;
 }
