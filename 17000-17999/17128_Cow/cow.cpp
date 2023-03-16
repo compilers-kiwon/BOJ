3d1dc7fbdc6da120d6938357c980c50f8709066a
Kiwon Kwon <compilers@naver.com>
2023-03-16 15:33:58 +0900

    https://www.acmicpc.net/problem/17128

---
+++ 17000-17999/17128_Cow/cow.cpp
@@ -0,0 +1,104 @@
+#include    <iostream>
+#include    <vector>
+
+using namespace std;
+
+#define get_idx(s,o,size)   (((s)+(o))%(size))
+
+typedef long long int   int64;
+
+typedef struct{
+    int64 value;
+    vector<int> related;  
+} Cow;
+
+static int  N,Q;
+
+int input(vector<Cow>& v,vector<int>& q)
+{
+    cin>>N>>Q;
+
+    for(int i=0;i<N;i++)
+    {
+        Cow c;
+
+        cin>>c.value;
+
+        c.related.push_back(get_idx(i,0,N));
+        c.related.push_back(get_idx(i,1,N));
+        c.related.push_back(get_idx(i,2,N));
+        c.related.push_back(get_idx(i,3,N));
+
+        v.push_back(c);
+    }
+
+    for(int i=0;i<Q;i++)
+    {
+        int n;
+
+        cin>>n;
+        q.push_back(n-1);
+    }
+
+    return  0;
+}
+
+int64   get_sum(const vector<Cow>& c,vector<int64>& sum)
+{
+    int64   ret = 0;
+
+    sum.resize(c.size(),1);
+
+    for(int i=0;i<c.size();i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            sum[c[i].related[j]] *= c[i].value;
+        }
+    }
+
+    for(int i=0;i<sum.size();i++)
+    {
+        ret += sum[i];
+    }
+
+    return  ret;
+}
+
+int64 do_query(int idx,vector<Cow>& c,vector<int64>& sum,int64 s)
+{
+    int64   ret = s;
+
+    for(int i=0;i<4;i++)
+    {
+        int64   from = sum[c[idx].related[i]];
+        int64   to = -sum[c[idx].related[i]];
+        
+        ret += to-from;
+        sum[c[idx].related[i]] *= -1;
+    }
+
+    return  ret;
+}
+
+int main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    vector<Cow>     C;
+    vector<int64>   S;
+    vector<int>     Q;
+    int64           sum;
+
+    input(C,Q);
+    sum = get_sum(C,S);
+
+    for(int i=0;i<Q.size();i++)
+    {
+        sum = do_query(Q[i],C,S,sum);
+        cout<<sum<<'\n';
+    }
+
+    return  0;
+}
