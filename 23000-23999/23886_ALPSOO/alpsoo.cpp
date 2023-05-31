0db6ff85bf931b71658bfa084a0891c0b669a76c
Kiwon Kwon <compilers@naver.com>
2023-05-31 17:45:25 +0900

    https://www.acmicpc.net/problem/23886

---
+++ 23000-23999/23886_ALPSOO/alpsoo.cpp
@@ -0,0 +1,60 @@
+#include    <iostream>
+#include    <string>
+#include    <vector>
+#include    <cmath>
+
+using namespace std;
+
+#define get_sign(n) ((n)/abs((n)))
+
+int get_inclination(const string& num,
+                        vector<int>& inc)
+{
+    for(int i=1;i<num.length();i++)
+    {
+        inc.push_back(num[i]-num[i-1]);
+    }
+
+    return  0;
+}
+
+bool    is_alpsoo(const vector<int>& inc)
+{
+    if( !(inc.front()>0 && inc.back()<0) )
+    {
+        return  false;
+    }
+
+    for(int i=1;i<inc.size();i++)
+    {
+        if( inc[i] == 0 )
+        {
+            return  false;
+        }
+
+        if( inc[i-1]!=inc[i] &&
+                get_sign(inc[i-1])==get_sign(inc[i]) )
+        {
+            return  false;
+        }
+    }
+
+    return  true;
+}
+
+int main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    string  X;
+    vector<int> inc;
+
+    cin>>X;
+    get_inclination(X,inc);
+
+    cout<<(is_alpsoo(inc)?
+        "ALPSOO":"NON ALPSOO")<<'\n';
+        
+    return  0;
+}
