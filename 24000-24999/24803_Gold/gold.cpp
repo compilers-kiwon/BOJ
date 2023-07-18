e341b2f1b6663ddabcb85ee0160825b5f7f1680c
Kiwon Kwon <compilers@naver.com>
2023-07-18 12:25:46 +0900

    https://www.acmicpc.net/problem/24803

---
+++ 24000-24999/24803_Gold/gold.cpp
@@ -0,0 +1,25 @@
+#include    <iostream>
+#include    <string>
+
+using namespace std;
+
+int main(void)
+{
+    int G,S,C,buying_power;
+
+    cin>>G>>S>>C;
+    buying_power = G*3+S*2+C*1;
+
+    string  v,t;
+
+    if (buying_power>=8) v="Province";
+    else if (buying_power>=5) v="Duchy";
+    else if (buying_power>=2) v="Estate";
+
+    if (buying_power>=6) t="Gold";
+    else if (buying_power>=3) t="Silver";
+    else t="Copper";
+
+    cout<<v<<(v.empty()?"":" or ")<<t<<'\n';
+    return  0;
+}
