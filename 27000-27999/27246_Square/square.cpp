e9c0a0471c4ff6e90fbd833a5f195d91218f4e32
Kiwon Kwon <compilers@naver.com>
2023-08-18 10:08:27 +0900

    https://www.acmicpc.net/problem/27246

---
+++ 27000-27999/27246_Square/square.cpp
@@ -0,0 +1,24 @@
+#include    <iostream>
+
+using namespace std;
+
+typedef long long int   int64;
+
+int main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int64   cnt,sum,n;
+
+    cin>>n;
+    sum = cnt = 0;
+
+    for(int64 i=1;;cnt++,i++)
+    {
+        if((sum+=i*i)>n) break;
+    }
+
+    cout<<cnt<<'\n';
+    return  0;
+}
