96ae3b2ae097bd9958ff921b912b8877e378ea24
Kiwon Kwon <compilers@naver.com>
2023-08-28 12:06:03 +0900

    https://www.acmicpc.net/problem/9214

---
+++ 09000-09999/09214_First/first.cpp
@@ -0,0 +1,43 @@
+#include    <iostream>
+#include    <string>
+
+using namespace std;
+
+string  get_prev(string& cur)
+{
+    if (cur.length()%2 != 0) return cur;
+
+    string  ret;
+
+    for (int i=0;i<cur.length();i+=2) {
+        char    n = cur[i];
+        char    c = cur[i+1];
+
+        for (int j=0;j<(int)(n-'0');j++) ret.push_back(c);
+    }
+
+    return  ret;
+}
+
+string  find_first(string& cur)
+{
+    string  prev = get_prev(cur);
+    return  (prev==cur)?prev:find_first(prev);
+}
+
+int main(void)
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    for (int i=1;;i++) {
+        string  s;
+
+        cin>>s;
+        if (s == "0") break;
+
+        cout<<"Test "<<i<<": "<<find_first(s)<<'\n';
+    }
+
+    return  0;
+}
