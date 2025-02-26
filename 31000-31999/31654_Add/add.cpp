6833cb74fda6187582edd11112dc30370bd5c53b
Kiwon Kwon <compilers@naver.com>
2025-02-26 16:04:06 +0900

    https://www.acmicpc.net/problem/31654

---
+++ 31000-31999/31654_Add/add.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int A, B, C;
+
+    cin>>A>>B>>C;
+    cout<<((A+B==C)?"correct!":"wrong!")<<'\n';
+
+    return 0;
+}
