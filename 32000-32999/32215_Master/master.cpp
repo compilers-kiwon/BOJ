ce3ab698953ca3dbcff24e05727be79e448ffe56
Kiwon Kwon <compilers@naver.com>
2025-02-26 14:10:32 +0900

    https://www.acmicpc.net/problem/32215

---
+++ 32000-32999/32215_Master/master.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int n, m, k;
+
+    cin>>n>>m>>k;
+    cout<<m*(k+1)<<'\n';
+
+    return 0;
+}
