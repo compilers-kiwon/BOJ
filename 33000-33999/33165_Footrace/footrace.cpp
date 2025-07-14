a423017e3af1d96e9476f864a32d631a44afb2b0
Kiwon Kwon <compilers@naver.com>
2025-07-14 22:22:04 +0900

    https://www.acmicpc.net/problem/33165

---
+++ 33000-33999/33165_Footrace/footrace.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int V, T;
+
+    cin >> V >> T;
+    cout << V * T <<'\n';
+
+    return 0;
+}
