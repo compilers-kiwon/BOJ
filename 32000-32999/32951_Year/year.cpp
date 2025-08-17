f475ec3626583f7a514f1b35a7a8841c18f14b8a
Kiwon Kwon <compilers@naver.com>
2025-08-17 15:49:08 +0900

    https://www.acmicpc.net/problem/32951

---
+++ 32000-32999/32951_Year/year.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+    cout << N - 2024 << '\n';
+
+    return 0;
+}
