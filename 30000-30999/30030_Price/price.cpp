4f00007273e8a5571731fcf8f724e337330f8adb
Kiwon Kwon <compilers@naver.com>
2025-09-01 19:55:10 +0900

    https://www.acmicpc.net/problem/30030

---
+++ 30000-30999/30030_Price/price.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int B;
+    cin >> B;
+    cout << (B / 11) * 10 << '\n';
+
+    return 0;
+}
