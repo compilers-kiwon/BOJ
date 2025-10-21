e70367c47d6c4588521c17bcc57940d65f22b969
Kiwon Kwon <compilers@naver.com>
2025-10-21 10:01:12 +0900

    https://www.acmicpc.net/problem/17987

---
+++ 17000-17999/17987_Howl/howl.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    string f;
+    cin >> f;
+    cout << f << "O\n";
+
+    return 0;
+}
