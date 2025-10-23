d23c2202944405aac002f1385dac1667d8f9156a
Kiwon Kwon <compilers@naver.com>
2025-10-23 20:03:02 +0900

    https://www.acmicpc.net/problem/34446

---
+++ 34000-34999/34446_Cart/cart.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int m, n, t;
+    cin >> m >> n >> t;
+    cout << 2 * m << '\n';
+
+    return 0;
+}
