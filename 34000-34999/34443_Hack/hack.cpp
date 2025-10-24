d9c03aafc1de9a113b14f44d63f8ecc062fe302b
Kiwon Kwon <compilers@naver.com>
2025-10-24 15:24:43 +0900

    https://www.acmicpc.net/problem/34443

---
+++ 34000-34999/34443_Hack/hack.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int N, C, P;
+    cin >> N >> C >> P;
+    cout << N * P << '\n';
+
+    return 0;
+}
