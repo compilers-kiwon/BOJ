4ceda8119cf91d02e34d570d83ffc7769ea8b0be
Kiwon Kwon <compilers@naver.com>
2025-10-30 15:13:49 +0900

    https://www.acmicpc.net/problem/34411

---
+++ 34000-34999/34411_Climb/climb.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int m, n, y;
+    cin >> m >> n >> y;
+    cout << m * n << '\n';
+
+    return 0;
+}
