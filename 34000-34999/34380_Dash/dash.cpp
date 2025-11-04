da57a4d4706093d9a2fbe80c30f276861f8e4da1
Kiwon Kwon <compilers@naver.com>
2025-11-04 17:23:51 +0900

    https://www.acmicpc.net/problem/34380

---
+++ 34000-34999/34380_Dash/dash.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int M, V, n, K;
+    cin >> M >> V >> n >> K;
+    cout << (2 * n) + 40 <<'\n';
+
+    return 0;
+}
