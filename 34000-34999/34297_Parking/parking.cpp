42f7f837e66fa3b5efec7521451f374877513c62
Kiwon Kwon <compilers@naver.com>
2025-11-14 14:31:10 +0900

    https://www.acmicpc.net/problem/34297

---
+++ 34000-34999/34297_Parking/parking.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int M, K, C;
+    cin >> M >> K >> C;
+    cout << M * C << '\n';
+
+    return 0;
+}
