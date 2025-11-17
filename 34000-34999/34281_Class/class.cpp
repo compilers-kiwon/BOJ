99aeb943280ba87b9be968638d4c9005a709ed9d
Kiwon Kwon <compilers@naver.com>
2025-11-17 11:46:14 +0900

    https://www.acmicpc.net/problem/34281

---
+++ 34000-34999/34281_Class/class.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int w, l;
+    cin >> w >> l;
+    cout << w * l << '\n';
+
+    return 0;
+}
