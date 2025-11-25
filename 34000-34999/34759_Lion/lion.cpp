879f3201f26c7dfcc94fb3614b5339ba5fea1a4f
Kiwon Kwon <compilers@naver.com>
2025-11-25 12:02:29 +0900

    https://www.acmicpc.net/problem/34759

---
+++ 34000-34999/34759_Lion/lion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long int int64;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int64 N;
+    cin >> N;
+    cout << 2 * N << '\n';
+
+    return 0;
+}
