3fea9b40e21b71f2feceee0f6f8bc49d5a08bf1d
Kiwon Kwon <compilers@naver.com>
2025-12-14 17:07:04 +0900

    https://www.acmicpc.net/problem/24086

---
+++ 24000-24999/24086_Height/height.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int A, B;
+    cin >> A >> B;
+    cout << B - A << '\n';
+
+    return 0;
+}
