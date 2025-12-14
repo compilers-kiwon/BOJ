1a8ce2ba45d06ce12c0e88f4e63fa29c8c42c2d5
Kiwon Kwon <compilers@naver.com>
2025-12-14 16:51:40 +0900

    https://www.acmicpc.net/problem/24082

---
+++ 24000-24999/24082_Cube/cube.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int x;
+    cin >> x;
+    cout << x * x * x << '\n';
+
+    return 0;
+}
