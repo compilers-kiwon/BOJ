67c3616afc5b5f8f8de587103ff95f11c1a94655
Kiwon Kwon <compilers@naver.com>
2025-12-16 12:10:11 +0900

    https://www.acmicpc.net/problem/31610

---
+++ 31000-31999/31610_Packing/packing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << (A * B) + C << '\n';
+
+    return 0;
+}
