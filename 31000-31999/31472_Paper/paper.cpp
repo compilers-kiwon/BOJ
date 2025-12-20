963e958194c1cc311c561f29d04f64e96fbf2f84
Kiwon Kwon <compilers@naver.com>
2025-12-20 21:09:06 +0900

    https://www.acmicpc.net/problem/31472

---
+++ 31000-31999/31472_Paper/paper.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int W;
+    cin >> W;
+    cout << 4 * sqrt(2 * W) << '\n';
+
+    return 0;
+}
