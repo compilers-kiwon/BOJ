b72bd886cdfff607caa419e43273a09e4a523f0f
Kiwon Kwon <compilers@naver.com>
2026-01-05 10:52:18 +0900

    https://www.acmicpc.net/problem/30010

---
+++ 30000-30999/30010_Sort/sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int N;
+
+    for (cin >> N; N > 0; N--) {
+        cout << N << ((N==1)?'\n':' ');
+    }
+
+    return 0;
+}
